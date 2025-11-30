4bebe8d17a01444b63189f71838d570de9e97686
Rodney Byun <rjbyun@gmail.com>
2025-11-30 00:28:38 -0500

    password validation policy

--- src/components/AccountForm.jsx
+++ src/components/AccountForm.jsx
@@ -1,20 +1,58 @@
 import React from "react";
+
+import PasswordPolicy from "./PasswordPolicy";
+
 function setPasswordInput(password, index) {
     const name = password.toLowerCase().replace(" ", "");
 
-    const pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
+    const pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+
+    const [showPolicy, setShowPolicy] = React.useState(false);
+    const [currentPass, setCurrentPass] = React.useState("");
     
+    const [isGoodLen, setIsGoodLen] = React.useState(false);
+    const [hasUpperCase, setHasUpperCase] = React.useState(false);
+    const [hasLowerCase, setHasLowerCase] = React.useState(false);
+    const [hasDigit, setHasDigit] = React.useState(false);
+    const digitRegex = /\d/;
+    const [hasSpecialChar, setHasSpecialChar] = React.useState(false);
+    const specialCharsRegex = /[@$!%*?&]/;
+
+    function passwordInput(event) {
+        const {value, name} = event.currentTarget;
+        setCurrentPass(value);
+        setIsGoodLen(value.length >= 8);
+        setHasUpperCase(value !== value.toLowerCase());
+        setHasLowerCase(value !== value.toUpperCase());
+        setHasDigit(digitRegex.test(value));
+        setHasSpecialChar(specialCharsRegex.test(value));
+
+    }
+
     return (
+        <>
         <input 
             className="account-input" 
             name={name} 
             id={name} 
-            type="password" 
+            type="password"
             placeholder={password} 
             pattern={pattern}
             key={index}
-            onFocus={event => console.log(event)}>
+            onChange={passwordInput}
+            value={currentPass}
+            onFocus={() => setShowPolicy(true)}
+            onBlur={() => setShowPolicy(false)}>
         </input>
+        <PasswordPolicy 
+            showPolicy={showPolicy} 
+            currentPass={currentPass} 
+            isGoodLen={isGoodLen}
+            hasUpperCase={hasUpperCase}
+            hasLowerCase={hasLowerCase}
+            hasDigit={hasDigit}
+            hasSpecialChar={hasSpecialChar}/>
+        </>
     )
 }
 
